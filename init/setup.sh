7eb7c14a227fe029a5620bba502652acae6343b3
umeruma <dev@umeru.ma>
2019-01-08 16:22:29 +0900

    Update setup.sh
    + Add chsh

--- init/setup.sh
+++ init/setup.sh
@@ -2,18 +2,24 @@
 unamestr=`uname`
 
 if type brew >/dev/null 2>&1; then
-echo "brew is installed";
+	echo "brew is installed";
 else
     if [[ "$unamestr" == 'Linux' ]]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+    	sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
     elif [[ "$unamestr" == 'Darwin' ]]; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
 fi
 
 if [ -f ~/.zplug/init.zsh ]; then
-echo "zplug is installed";
+	echo "zplug is installed"
 else
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-sudo chmod -R 755 ~/.zplug
+	curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+	sudo chmod -R 755 ~/.zplug
+fi
+
+if [[ "$SHELL" == "/bin/zsh" ]]; then
+else
+	echo "Change default shell to /bin/zsh"
+	chsh -s /bin/zsh
 fi
