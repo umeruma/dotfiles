0b3701f596b2d20ac89feb9affe125323d37f57a
umeruma <dev@umeru.ma>
2025-09-27 22:08:37 +0900

    feat!: add website/ for docs

---
+++ website/astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'umeruma dotfiles',
+      social: [{
+        icon: 'github',
+        label: 'GitHub',
+        href: 'https://github.com/umeruma/dotfiles' 
+      }],
+      sidebar: [
+        { label: 'home', slug: 'index' },
+        {
+          label: 'etc',
+          items: [
+            { label: 'Setup Hardware', slug: 'etc/setup-hard' },
+          ],
+        },
+      ],
+    }),
+  ],
+});
