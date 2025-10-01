46dbaef8c84761f2d69830e8e297362bc18a8887
umeruma <dev@umeru.ma>
2025-10-01 18:14:17 +0900

    fix: remove tea in setup script

--- init/setup.sh
+++ init/setup.sh
@@ -1,25 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 unamestr=$(uname)
 
 if type brew >/dev/null 2>&1; then
     echo "brew is installed";
 else
-    if [[ "$unamestr" == 'Linux' ]]; then
+    if [ "$unamestr" = 'Linux' ]; then
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-    elif [[ "$unamestr" == 'Darwin' ]]; then
+    elif [ "$unamestr" = 'Darwin' ]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
 fi
 
-if [ -f /usr/local/bin/tea ]; then
-    echo "tea is installed";
-else
-    if [[ "$unamestr" == 'Darwin' ]]; then
-        sh <(curl https://tea.xyz)
-    fi
-fi
-
-if [[ "$SHELL" == "/bin/zsh" ]]; then
+if [ "$SHELL" = "/bin/zsh" ]; then
     echo "Using /bin/zsh"
 else
     echo "Change default shell to /bin/zsh"
