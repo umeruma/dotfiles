34d28a03cd01aef1d8e34c386e71c4ed52f64d5a
umeruma <dev@umeru.ma>
2026-07-10 15:36:39 +0900

    feat: add Doom Emacs setup and dependencies
    Introduced a new setup task for Doom Emacs in mise.toml, including installation scripts for macOS and Windows. Updated the Brewfile to include necessary dependencies for Doom Emacs, and modified the .zprofile to add the Doom Emacs CLI to the PATH. Additionally, updated the deployment subdirectory and documentation to reflect these changes.

---
+++ home/.config/doom/packages.el
@@ -0,0 +1,7 @@
+;;; $DOOMDIR/packages.el -*- lexical-binding: t; no-byte-compile: t; -*-
+
+;; To install a package:
+;;
+;;   1. Declare them here in a `package!' statement,
+;;   2. Run 'doom sync' in the shell,
+;;   3. Restart Emacs.
